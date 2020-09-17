e7794a7a68a333bb8d5924483c2479b4d40cc7d6
Weirong Xu <weirongxu.raidou@gmail.com>
2020-09-17 16:02:15 +0800

    fix: JestHelper

--- JestHelper.js
+++ JestHelper.js
@@ -1,3 +1,4 @@
 const { JestHelper } = require('./lib/cjs/JestHelper.js');
+const path = require('path');
 exports.JestHelper = JestHelper;
 exports.jestHelper = new JestHelper(path.join(__dirname, 'tests'));
