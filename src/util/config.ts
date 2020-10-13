aa60c792ffc6f983804a6c30c9b956d88994c2a5
Weirong Xu <weirongxu.raidou@gmail.com>
2020-10-14 01:07:37 +0800

    refactor: getConfigLocal

--- src/util/config.ts
+++ src/util/config.ts
@@ -1,5 +1,6 @@
 import { workspace } from 'coc.nvim';
 import { generateUri } from './text';
 
-export const configLocal = (resource: string = generateUri(workspace.cwd)) =>
-  workspace.getConfiguration('explorer', resource);
+export const getConfigLocal = (section: string) => (
+  resource: string = generateUri(workspace.cwd),
+) => workspace.getConfiguration(section, resource);
